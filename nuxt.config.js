0f5ddb6b4bbee570fdf69aff0d802702cec6678e
kazu1192 <dye.deep.red@gmail.com>
2019-11-29 23:47:00 +0900

    Add: 動的にページ遷移、v-forを使った一覧表示、記事ページ、md変換
    デプロイしたmarkdownファイルから動的にページを作成する。
    
    Todo: もしくは、generateを活用する。

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,15 +30,13 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['@/plugins/markdown-it'],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
